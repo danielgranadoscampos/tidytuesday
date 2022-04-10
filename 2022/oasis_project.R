01385e95867a2b2cc72684231ec979d1e67bda97
danielgranadoscampos <granadoscampos.daniel@gmail.com>
2022-04-10 16:49:30 -0600

    additional label changes

--- 2022/oasis_project.R
+++ 2022/oasis_project.R
@@ -23,9 +23,13 @@ p <- ggplot(news_data, aes(x=year_founded, group=products, fill=products)) +
        title = "How news products have changed based on the year a publication is founded",
        caption = "Data:Project Oasis") +
   theme_bw() +
-  theme(plot.title = element_text(size = 30))
+  theme(plot.title = element_text(size = 30),
+        axis.title = element_text(size = 22),
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title =  element_text(size = 14) ) +
   scale_fill_brewer(palette = "Set3")
 
 
-ggsave("oasis_project.png", plot = p, width = 350, height = 300, units = "mm", dpi = "retina")
+ggsave("oasis_project.png", plot = p, width = 430, height = 300, units = "mm", dpi = "retina")
 
